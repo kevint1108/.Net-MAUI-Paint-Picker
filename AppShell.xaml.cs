72bec50f80e7812396b3cf5a5feec44bb864196d
Binh Tang <terikn1107@gmail.com>
2025-05-19 23:13:49 -0700

    Add files via upload

---
+++ AppShell.xaml.cs
@@ -0,0 +1,10 @@
+﻿namespace PaintPicker
+{
+    public partial class AppShell : Shell
+    {
+        public AppShell()
+        {
+            InitializeComponent();
+        }
+    }
+}
---
+++ MainPage.xaml.cs
@@ -0,0 +1,55 @@
+﻿using System.Diagnostics;
+
+namespace PaintPicker
+
+{
+    public partial class MainPage : ContentPage
+    {
+        bool isRandom;
+        public MainPage()
+        {
+            InitializeComponent();
+        }
+
+        private void slider_ValueChanged(object sender, ValueChangedEventArgs e)
+        {
+            if (!isRandom)
+            {
+                var red = sliderRed.Value;
+                var green = sliderGreen.Value;
+                var blue = sliderBlue.Value;
+
+                Color color = Color.FromRgb(red, green, blue);
+
+                SetColor(color);
+            }
+        }
+
+        private void SetColor(Color color)
+        {
+            // Debug.WriteLine(color.ToString());
+            btnRandom.Background = color;
+            Container.Background = color;
+            lblHex.Text = color.ToHex();
+        }
+
+        private void btnRandom_Clicked(object sender, EventArgs e)
+        {
+            isRandom = true;
+            var random = new Random();
+
+            var color = Color.FromRgb(
+                random.Next(0, 256),
+                random.Next(0, 256),
+                random.Next(0, 256));
+
+            SetColor(color);
+
+            sliderRed.Value = color.Red;
+            sliderGreen.Value = color.Green;
+            sliderBlue.Value = color.Blue;
+            isRandom = false;
+        }
+    }
+
+}
